5754c0ce8ae1230931afef846bc16d3bf1229540
alijah <alijahjv@gmail.com>
2018-06-26 13:44:23 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+// filter
+$(document).ready(function() {
+	//Display the filters
+	$('#filter').on('click', function() {
+		$(this).toggleClass('active');
+		$('.filter').fadeToggle('#filter');
+		
+	});
+	
+	
+	// Filter the content
+	$('#breakfast').on('click', function() {
+		$(this).closest('#wrapper').find('.breakfast').toggleClass('hide');
+	});
+	$('#lunch').on('click', function() {
+		$(this).closest('#wrapper').find('.lunch').toggleClass('hide');
+	});
+	$('#dinner').on('click', function() {
+		$(this).closest('#wrapper').find('.dinner').toggleClass('hide');
+	});
+	$('#snacks').on('click', function() {
+		alert('No Snacks right now!!!');
+		});
+	//Button Recipe
+	
+	$('article').on('click', function() {
+		$(this).find('.content').toggleClass('hide');
+		
+	});
+	
+	
+	
+	
+});
